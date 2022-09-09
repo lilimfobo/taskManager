1e1731a6399f607c9112ed1275e54a1c06421296
Lili <102562771+lilimfobo@users.noreply.github.com>
2022-09-09 23:31:24 +0200

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,12 +1,21 @@
-// window.addEventListener('load', () => {
-// 	todos = JSON.parse(localStorage.getItem('todos')) || [];
-// 	const enterName = document.querySelector('#yourName');
-// 	const newForm = document.querySelector('#createTask');
+window.addEventListener('load', () => {
+	todos = JSON.parse(localStorage.getItem('todos')) || [];
+	const enterName = document.querySelector('#yourName');
+	const newForm = document.querySelector('#createTask');
 
-// 	const myName = localStorage.getItem('myName') || '';
+	const myName = localStorage.getItem('myName') || '';
 
-// 	enterName.value = username;
+	enterName.value = myName;
 
-// 	enterName.addEventListener('change', (e) => {
-// 		localStorage.setItem('myName', e.target.value);
-// 	})
+	enterName.addEventListener('change', (e) => {
+		localStorage.setItem('myName', e.target.value);
+	})
+
+    nameInput.value = myName;
+
+	nameInput.addEventListener('change', (e) => {
+		localStorage.setItem('myName', e.target.value);
+	})
+
+	newForm.addEventListener('submit', e => {
+		e.preventDefault();
