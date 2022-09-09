422dc8beb39d27a1de7a8a5032fd5a48c435e133
Lili <102562771+lilimfobo@users.noreply.github.com>
2022-09-09 23:28:15 +0200

    Update javascript.js

---
+++ js/javascript.js
@@ -0,0 +1,12 @@
+// window.addEventListener('load', () => {
+// 	todos = JSON.parse(localStorage.getItem('todos')) || [];
+// 	const enterName = document.querySelector('#yourName');
+// 	const newForm = document.querySelector('#createTask');
+
+// 	const myName = localStorage.getItem('myName') || '';
+
+// 	enterName.value = username;
+
+// 	enterName.addEventListener('change', (e) => {
+// 		localStorage.setItem('myName', e.target.value);
+// 	})
